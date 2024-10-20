6a0f3b1220286a56a455d6b9851fdbc1c7cf20f6
joudy003 <joudy.j.t@gmail.com>
2024-10-20 15:49:04 +0300

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Joud Abdullah AlAmri on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipeTests/RecipeTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeTests.swift
+//  RecipeTests
+//
+//  Created by Joud Abdullah AlAmri on 10/20/24.
+//
+
+import Testing
+@testable import Recipe
+
+struct RecipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
